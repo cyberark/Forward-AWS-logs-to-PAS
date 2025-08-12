6aa53ed6b741cb5ff22c4117347790c716bb878a
vikicybr <155361555+vikicybr@users.noreply.github.com>
2025-08-12 18:35:03 +0300

    Update lambda_function.py

--- src/MySnsToPta/lambda_function.py
+++ src/MySnsToPta/lambda_function.py
@@ -1,19 +1,19 @@
 import json
 import socket
 import os
-from aws_syslog_mapper import normalize_aws_syslog  # our mapping function
+from typing import Dict, Any
+from aws_syslog_mapper import normalize_aws_syslog
 
 TCP_IP = os.environ['PTAIP']
 TCP_PORT = int(os.environ['PTAPort'])  # PTA Port
-BUFFER_SIZE = 1024
 
-def sendData(sock, msg):
+def sendData(sock: socket.socket, msg: bytes):
     try:
         sock.sendall(msg)
     except:
         print("Failed to send data!")
 
-def lambda_handler(event, context):
+def lambda_handler(event: Dict[str, Any], context: Any):
     print(json.dumps(event))  # For debugging
 
     for record in event["Records"]:
@@ -21,7 +21,6 @@ def lambda_handler(event, context):
             message = parse_and_transform(record)
             print(f"Transformed message: {message}")
 
-            # Open TCP connection
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 print(f"Connecting to PTA at {TCP_IP}:{TCP_PORT}")
                 sock.connect((TCP_IP, TCP_PORT))
@@ -37,10 +36,10 @@ def lambda_handler(event, context):
         'body': json.dumps('Lambda to PTA completed successfully!')
     }
 
-def parse_and_transform(record):
+def parse_and_transform(record: Dict[str, Any]) -> str:
     print(f"Record data: {record}")
 
-    if record.get("EventSource") == "aws:sns":
+    if record.get("EventSource") == 'aws:sns':
         print("The event source is aws:sns")
         sns_message = record["Sns"]["Message"]
     else:
@@ -53,5 +52,4 @@ def parse_and_transform(record):
         raise ValueError(f"Invalid JSON in SNS message: {e}")
 
     transformed_event = normalize_aws_syslog(cloudtrail_event)
-
     return json.dumps(transformed_event)
